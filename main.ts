adab40728f697ace0597982e885d9de57a27f975
MillenniumEarl <22562005+MillenniumEarl@users.noreply.github.com>
2021-08-24 13:35:37 +0200

    Code refactored

--- main.ts
+++ main.ts
@@ -1,9 +1,9 @@
 import electron from 'electron';
 const { app, BrowserWindow, protocol, net } = electron;
-import fs from 'fs';
+import { promises as fs } from 'fs';
 import moment from 'moment';
 import express from 'express';
-import { readFileSync } from 'original-fs';
+import path from 'path';
 
 interface ICaptchaData {
 	/**
@@ -24,11 +24,45 @@ interface ICaptchaData {
 	sitekey: string;
 }
 
-const captchaBank: ICaptchaData[] = [];
+// Global consts
+const EXPIRE_TOKEN_VALUE = 120;
+const TARGET_WEBSITE = 'http://supremenewyork.com/';
+const TARGET_WEBSITE_KEY = '6LeWwRkUAAAAAOBsau7KpuC9AV-6J8mhw4AjC3Xz';
+
+// Global variables
+let captchaBank: ICaptchaData[] = [];
+
+// Set a timer for expired token removal
+setInterval(removeExpiredTokens, 1000);
+
+// Start the Express server
+const server = initBankServer();
 
 //#region Methods
+/**
+ * Sleep for `ms` milliseconds.
+ * @param ms Number of milliseconds to wait
+ */
 function sleep(ms:number) {
 	return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+/**
+ * Remove from the bank expired tokens.
+ */
+function removeExpiredTokens() {
+	// Save the current length of the array
+	const startLenght = captchaBank.length;
+	
+	captchaBank = captchaBank.filter((captcha) => {
+		const timestamp = moment().diff(moment(captcha.timestamp), 'seconds');
+		const isExpired = timestamp > EXPIRE_TOKEN_VALUE;
+		return !isExpired;
+	});
+
+	// Show message only if a token was removed
+	const diff = startLenght - captchaBank.length;
+	if (diff !== 0) console.log(`Removing ${diff} Expired Captcha Token`);
 }
 
 async function initCaptchaWindow() {
@@ -39,52 +73,60 @@ async function initCaptchaWindow() {
 				allowRunningInsecureContent: true
 			}
 	});
-	SetupIntercept();
+	setupIntercept();
 
+	//captchaWindow.loadURL(TARGET_WEBSITE);
 	captchaWindow.loadURL('https://accounts.google.com');
 	
 	await sleep(1000);
 
-	captchaWindow.webContents.session.webRequest.onBeforeRequest({urls: ['https://myaccount.google.com/*']}, (_details, callback) => {
-		callback({redirectURL: 'http://supremenewyork.com/'})
-	})
+	captchaWindow.webContents.session.webRequest.onBeforeRequest(
+		{urls: ['https://myaccount.google.com/*']}, 
+		(_details, callback) => callback({redirectURL: TARGET_WEBSITE}));
 };
 
-function SetupIntercept() {
-	protocol.interceptBufferProtocol('http', (req, callback) => {
-		if(req.url == 'http://supremenewyork.com/') {
-			fs.readFile(__dirname + '/captcha.html', 'utf8', function(_err, html){
-				//callback({mimeType: 'text/html', data: Buffer.from(html)});
-				callback(Buffer.from(html));
-			});
-		}else{
-			const request = net.request(req)
+function setupIntercept() {
+	/**
+	 * Write data in a request.
+	 */
+	async function writeUploadData(data: electron.UploadData[], request: electron.ClientRequest) {
+		// Obtains the data from the request
+		const promises = data
+		.filter((part) => part.bytes || part.file)
+		.map(async (part) => part.bytes ?? await fs.readFile(part.file));
+
+		// Await for all the buffers to be read
+		const parts = await Promise.all(promises);
+
+		// Write buffers to request
+		parts.forEach((part) => request.write(part));
+	}
+	async function handler(req: electron.InterceptBufferProtocolRequest, callback: (buffer?: Buffer | undefined) => void) {
+		if(req.url === TARGET_WEBSITE) {
+			// Load captcha
+			console.log("Loading CAPTCHA page");
+			const filename = path.join(__dirname, 'captcha.html');
+			const html = await fs.readFile(filename, 'utf8');
+			callback(Buffer.from(html));
+		}
+		else {
+			console.log(`Receiving request from: ${req.url}`);
+			const request = net.request(req);
+
 			request.on('response', res => {
-				const chunks: Buffer[] = []
+				const chunks: Buffer[] = [];
 	
-				res.on('data', chunk => {
-					chunks.push(Buffer.from(chunk))
-				})
-	
-				res.on('end', async () => {
-					const file = Buffer.concat(chunks)
-					callback(file)
-				})
+				res.on('data', chunk => chunks.push(Buffer.from(chunk)));
+				res.on('end', () => callback(Buffer.concat(chunks)));
 			})
 	
-			if (req.uploadData) {
-				req.uploadData.forEach(part => {
-					if (part.bytes) {
-						request.write(part.bytes)
-					} else if (part.file) {
-						request.write(readFileSync(part.file))
-					}
-				})
-			}
+			if (req.uploadData) await writeUploadData(req.uploadData, request);
 	
-			request.end()
+			request.end();
 		}
-	})
+	}
+
+	protocol.interceptBufferProtocol('http', handler);
 };
 
 /**
@@ -108,8 +150,13 @@ function initBankServer(port:number = 8080) {
 		// Return all the reCAPTCHAs' tokens
 		return res.json(captchaBank),
 
-		// Remove the first token in the bank
-		captchaBank.splice(0,1);
+		// Remove all the token from bank
+		captchaBank = [];
+	});
+
+	bankExpressApp.get('/latest', function(_req, res) {
+		// Return the last token
+		return res.json(captchaBank.pop());
 	});
 
 	return bankExpressApp.listen(bankExpressApp.get('port'));
@@ -120,28 +167,22 @@ function initBankServer(port:number = 8080) {
 //#region Electron IPC
 app.on('ready', () => initCaptchaWindow());
 
+app.on('window-all-closed', () => {
+	// Close the server
+	server.close();
+
+	// Close the app
+  	app.quit();
+});
+
 electron.ipcMain.on('openCapWindow', () => initCaptchaWindow());
 
-electron.ipcMain.on('sendCaptcha', function(token: string) {
+electron.ipcMain.on('sendCaptcha', function(_: any, token: string) {
 	captchaBank.push({
-	  token: token,	
+	  token: token,
 	  timestamp: moment(),
-	  host: 'http://supremenewyork.com/',
-	  sitekey: '6LeWwRkUAAAAAOBsau7KpuC9AV-6J8mhw4AjC3Xz'
-	})
+	  host: TARGET_WEBSITE,
+	  sitekey: TARGET_WEBSITE_KEY
+	});
 });
 //#endregion Electron IPC
-
-
-
-
-setInterval(function() {
-	for (var i = 0; i < captchaBank.length; i++) {
-		if (moment().diff(moment(captchaBank[i].timestamp), 'seconds') > 110) {
-			console.log('Removing Expired Captcha Token')
-			captchaBank.splice(0,1)
-		}
-	}
-}, 1000);
-  
-const server = initBankServer();
